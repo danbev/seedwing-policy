e4075578ce90ec2c7a050f4908154f9226cc9e43
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-05-08 09:39:27 +0200

    Add initial policy engine eval function
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- engine/js/index.mjs
+++ engine/js/index.mjs
@@ -1,3 +1,13 @@
 import { engine  } from './dist/seedwing_policy-engine-component.js';
 
 console.log(`Seedwing Policy Engine version: ${engine.version()}`);
+const policies = []; // The wit enabled engine does not support this yet.
+const datas = [];    // The wit enabled engine does not support this yet.
+const policy = 'pattern dog = { name: string, trained: boolean }';
+const name = "dog"
+const input = JSON.stringify({
+  name: "goodboy",
+  trained: true
+});
+const result = engine.eval(policies, datas, policy, name, input);
+console.log(result);
--- engine/src/wit.rs
+++ engine/src/wit.rs
@@ -1,3 +1,7 @@
+use crate::lang::builder::Builder;
+use crate::runtime::sources::Ephemeral;
+use crate::runtime::RuntimeError;
+use crate::runtime::{EvalContext, EvaluationResult};
 use crate::wit::engine::Engine;
 
 wit_bindgen::generate!("engine");
@@ -7,6 +11,44 @@ struct Exports;
 impl Engine for Exports {
     fn version() -> String {
         crate::version().to_string()
+    }
+
+    fn eval(
+        policies: Vec<String>,
+        data: Vec<String>,
+        policy: String,
+        name: String,
+        input: String,
+    ) -> String {
+        println!("policies: {policies:?}");
+        println!("data: {data:?}");
+        println!("policy: {policy:?}");
+        println!("name: {name:?}");
+        println!("input: {input:?}");
+
+        let mut builder = Builder::new();
+        // TODO: could we perhaps add a DataSource which is just a hashmap with
+        // a name as the key and the value is the policy as a string?
+        //builder.data(DirectoryDataSource::new(test_data_dir()));
+        let res = builder.build(Ephemeral::new("wit", policy).iter()).unwrap();
+        futures::executor::block_on(async {
+            let runtime = builder.finish().await;
+            let result = runtime
+                .unwrap()
+                .evaluate(format!("wit::{name}"), input, EvalContext::default())
+                .await;
+            //Ok(result)
+            //Ok::<Result<EvaluationResult, RuntimeError>, E>(result)
+            println!("result: {result:?}");
+        });
+
+        //let runtime = builder.finish().await.unwrap();
+        /*
+        let result = runtime
+            .evaluate("test::test-pattern", value, EvalContext::default())
+            .await;
+        */
+        "eval result....".to_string()
     }
 }
 
